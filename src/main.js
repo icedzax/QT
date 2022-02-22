91165f1a5ac786023111efcc703aa17f3c79e70e
icedzax <your_email_address@icedza@gmail.com>
2022-02-22 09:52:13 +0700

    minor fix

--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
 import { createApp } from "vue";
 import "./tailwind.css";
 import App from "./App.vue";
-import { routes } from "./routes.js";
-import { createRouter, createWebHistory } from "vue-router";
-import axios from "axios";
+import router from "./routes";
 
 const app = createApp(App);
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-  axios,
-});
 
 app.use(router);
 app.mount("#app");
