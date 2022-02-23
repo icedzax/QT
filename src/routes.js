354b83dd4117fd8e4a6720e7fa6e9719336d32f1
icedzax <your_email_address@icedza@gmail.com>
2022-02-23 13:50:33 +0700

    routes fix

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../src/views/Home.vue";
-import Main from "../src/views/Main.vue";
+import Main from "./views/Main.vue";
 import Set from "../src/components/Set.vue";
 import { auth } from "./state/user";
 const routes = [
