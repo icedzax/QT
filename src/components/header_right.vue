fc162d8389f89db677232c0da3702f28588fb1ec
sirikarn sangjae <siirikarn.s@zubbsteel.com>
2022-03-12 13:25:59 +0700

    DIT-132 edit header after price type

--- src/components/header_right.vue
+++ src/components/header_right.vue
@@ -9,23 +9,23 @@
         </p>
       </div>
       <div
-        class="grid overflow-hidden grid-cols-2 grid-rows-2 gap-1 mx-16 lg:mx-0 text-left mt-1"
+        class="grid overflow-hidden grid-cols-3 grid-rows-2 gap-1 mx-16 lg:mx-0 text-left mt-1"
       >
-        <div class="row-start-1 row-span-1 col-start-1 col-span-1">
+        <div class="row-start-1 row-span-1 col-start-1 col-span-1 p-1">
           เลขที่ใบเสนอราคา
         </div>
-        <div class="row-start-1 row-span-1 col-start-2 col-span-1 xl:pl-8">
+        <div class="row-start-1 row-span-1 col-start-2 col-span-2 p-1">
           ZQ-X01-6305005
           <button
-            class="text-center rounded-full p-1 px-2 text-sm border-2 border-green-500 text-black hover:text-green-600 font-semibold shadow-lg ring-1 ring-green-200"
+            class="text-center rounded-full px-2 text-sm border-2 border-green-500 text-black hover:text-green-600 font-semibold shadow-lg ring-1 ring-green-200"
           >
             บันทึก
           </button>
         </div>
-        <div class="row-start-2 row-span-1 col-start-1 col-span-1">
+        <div class="row-start-2 row-span-1 col-start-1 col-span-1 p-1">
           วันที่เสนอราคา
         </div>
-        <div class="row-start-2 row-span-1 col-start-2 col-span-1 xl:pl-8">
+        <div class="row-start-2 row-span-1 col-start-2 col-span-1 p-1">
           11/05/2020
         </div>
       </div>
