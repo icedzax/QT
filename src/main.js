ffbed7aace258e4fd587ab5d6e2bda6da2213fc2
sirikarn sangjae <siirikarn.s@zubbsteel.com>
2022-03-29 08:05:00 +0700

    DIT-175 add search box in List view

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import router from "./routes";
 import SimpleTypeahead from "vue3-simple-typeahead";
 import "vue3-simple-typeahead/dist/vue3-simple-typeahead.css";
 import VNum from "v3-num";
-
+// import Vuetify from "vuetify";
+// import "vuetify/dist/vuetify.min.css";
 const app = createApp(App);
 
 app.use(VNum);
 app.use(router);
 app.use(SimpleTypeahead);
 app.mount("#app");
+// app.use(Vuetify);
+// const opts = {};
+
+// export default new Vuetify(opts);
