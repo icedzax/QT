53e498ec25b519f232c8e4303223c5dd5dc05bdf
icedzax <your_email_address@icedza@gmail.com>
2022-05-31 10:33:34 +0700

    - unuse func

--- src/App.vue
+++ src/App.vue
@@ -62,22 +62,7 @@ export default {
       localStorage.setItem("tempqt", null);
     }
   },
-  methods: {
-    addComma(a) {
-      let x = a.split(".");
-      let x1 = x[0];
-
-      let x2 = x.length > 1 ? "." + x[1] : "";
-
-      var rgx = /(\d+)(\d{3})/;
-
-      while (rgx.test(x1)) {
-        x1 = x1.replace(rgx, "$1" + "," + "$2");
-      }
-      let aa = x1 + x2;
-      return aa;
-    },
-  },
+  methods: {},
 };
 </script>
 <style></style>
